cd7fb065a5d80a461dc159a726387448cbea1b05
mysthkr4036@gmail.com <hikaru_miyashita>
2023-02-21 13:28:35 +0900

    Edit login session and define helper mothod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+  before_action :login_required
+  
+  private
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
   resources :pictures
 end
