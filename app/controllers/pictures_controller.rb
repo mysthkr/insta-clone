9b786fb24542950130da27211e34e249d90299d0
mysthkr4036@gmail.com <hikaru_miyashita>
2023-02-21 11:25:24 +0900

    Create app and set up configurations.Create db, controller of pictures, views of pictures.

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,13 @@
+class PicturesController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'pictures/index'
+  get 'pictures/edit'
+  get 'pictures/new'
+  get 'pictures/show'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
