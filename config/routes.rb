0ffe6654d81e2895125de8376959fe102ae8169e
mysthkr4036@gmail.com <hikaru_miyashita>
2023-02-21 11:26:33 +0900

    Add routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'pictures/index'
-  get 'pictures/edit'
-  get 'pictures/new'
-  get 'pictures/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :pictures
 end
