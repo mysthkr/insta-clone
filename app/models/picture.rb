0c6cce473fb5cfa4ff5b75b8ed33ed7df5c50952
mysthkr4036@gmail.com <hikaru_miyashita>
2023-02-22 10:05:19 +0900

    Add validation of picture model.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,6 @@
 class Picture < ApplicationRecord
+  validates :image,  presence: true
+  validates :comment, presence: true, length: { maximum: 255 }
   mount_uploader :image, ImageUploader
   has_many :favorites, dependent: :destroy
   belongs_to :user
